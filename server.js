98125d69fe1e2b81d94ec9f639c107d1ad21aa10
newsupernova0617 <7456namu@yonsei.ac.kr>
2025-11-10 15:25:17 +0900

    variable update2

--- server.js
+++ server.js
@@ -22,11 +22,10 @@ function validateEnvironment() {
   const requiredEnvVars = [
     'JWT_SECRET',
     'ADMIN_PASSWORD',
-    'R2_PUBLIC_DOMAIN',
-    'R2_BUCKET_NAME',
+    'R2_ENDPOINT',
+    'R2_BUCKET',
     'R2_ACCESS_KEY_ID',
-    'R2_SECRET_ACCESS_KEY',
-    'R2_ACCOUNT_ID'
+    'R2_SECRET_ACCESS_KEY'
   ];
 
   const missing = requiredEnvVars.filter(varName => !process.env[varName]);
